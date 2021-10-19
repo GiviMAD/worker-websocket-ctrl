13b16bddf05efd20313eec7c22fd72b0541d73a8
Miguel <miguelwork92@gmail.com>
2021-10-19 17:13:30 +0200

    0.1.0: emit disconnect and api refactor

--- worker-websocket-controller.ts
+++ worker-websocket-controller.ts
@@ -11,16 +11,20 @@ interface WorkerWebSocketCtrl {
     /**
      * Listen websocket connection/reconnection.
      */
-    onConnect: () => void;
+    onConnect?: () => void;
+    /**
+    * Listen websocket connection/reconnection.
+    */
+    onDisconnect?: () => void;
     /**
      * Called once when health check fails for this subscription meaning 
      * it has been removed and no events will be send to it.
      */
-    onClose: () => void;
+    onClose?: () => void;
 }
 let ws = null as WebSocket | null;
 let subscribers = new Set<WorkerWebSocketCtrl>();
-let _getProtocols: (() => Promise<string[] | null>) | null = null;
+let _getProtocols: ((path: string) => Promise<string[] | null>) | null = null;
 const DEFAULT_KEEPALIVE_INTERVAL_SEG = 30;
 const DEFAULT_KEEPALIVE_TIMEOUT_SEG = 5;
 const DEFAULT_RECONNECT_SEG = 5;
@@ -39,7 +43,7 @@ export class WorkerWsController {
     private keepAliveTimeout: number;
     private reconnectDelay: number;
     private closeDelay: number;
-    constructor(private workerCtx: Window, private path: string, private options: WorkerWsCtrlOptions = {}) {
+    constructor(private workerCtx: Window, public path: string, private options: WorkerWsCtrlOptions = {}) {
         if (path.startsWith('/')) {
             this.path = path.substring(1);
         }
@@ -48,15 +52,15 @@ export class WorkerWsController {
         this.reconnectDelay = (options.reconnectDelay ?? DEFAULT_RECONNECT_SEG) * 1000;
         this.closeDelay = (options.closeDelay ?? 0) * 1000;
     }
-    start(apiUrl: string, getProtocols?: () => Promise<string[] | null>) {
+    start(apiUrl: string, getProtocols?: (path: string) => Promise<string[] | null>) {
         this.apiUrl = apiUrl;
         this.apiUrlToJoin = apiUrl.endsWith('/') ? apiUrl.substring(0, apiUrl.length - 1) : apiUrl;
         _getProtocols = getProtocols ?? null;
         if (ws != null) return;
         this.startWs();
     }
-    subscribe(onEvent: (ev: string) => void, ping: (pong: () => void) => void, onConnect: () => void, onClose: () => void) {
-        const s = { onEvent, ping, onConnect, onClose };
+    subscribe(onEvent: (ev: string) => void, ping: (pong: () => void) => void, onConnect?: () => void, onDisconnect?: () => void, onClose?: () => void) {
+        const s = { onEvent, ping, onConnect, onDisconnect, onClose };
         subscribers.add(s);
         const keepAliveRef = this.keepAlive(s);
         const unsubscribe = Comlink.proxy(() => {
@@ -78,7 +82,7 @@ export class WorkerWsController {
     private async startWs() {
         if (ws) return;
         try {
-            const protocols = await _getProtocols?.();
+            const protocols = await _getProtocols?.(this.path);
             if (_getProtocols && !protocols) return this.startReconnect();
             const socket = ws = this.openSocket(protocols ?? undefined);
             socket.onmessage = (msg) => {
@@ -86,9 +90,10 @@ export class WorkerWsController {
             };
             socket.onopen = () => {
                 this.stopReconnect();
-                subscribers.forEach(s => s.onConnect());
+                subscribers.forEach(s => s.onConnect?.());
             };
             socket.onclose = () => {
+                subscribers.forEach(s => s.onDisconnect?.());
                 this.startReconnect();
             };
         } catch (error) {
@@ -118,11 +123,10 @@ export class WorkerWsController {
             const timeoutRef = setTimeout(() => {
                 subscribers.delete(s);
                 clearInterval(intervalRef);
-                s.onClose();
+                s.onClose?.();
                 this.checkSubscriptions();
             }, this.keepAliveTimeout);
             s.ping(Comlink.proxy(() => {
-                // console.log('pong');
                 clearTimeout(timeoutRef);
             }));
         }, this.keepAliveInterval);
--- worker-ws-subscriptions.ts
+++ worker-ws-subscriptions.ts
@@ -1,16 +1,21 @@
 import * as Comlink from 'comlink';
 import type { WorkerWsController } from './worker-websocket-controller';
-
 export class WorkerWsSubscriptions<T extends string = string> {
     private api: string | (() => string);
     private createWorker: (resource: T) => Worker | SharedWorker | undefined;
-    private getProtocolsProxy?: (() => Promise<string[] | null>) & Comlink.ProxyMarked;
+    private getProtocolsProxy?: ((path: string) => Promise<string[] | null>) & Comlink.ProxyMarked;
     private autoUnsubscribeBinded?: ((this: WorkerWsSubscriptions<T>, ctx: unknown, unsubscribe: () => void) => void);
-    constructor(options: { api: string | (() => string), createWorker: (resource: T) => Worker | SharedWorker | undefined, getProtocols?: () => Promise<string[] | null>, autoUnsubscribe?: (this: WorkerWsSubscriptions<T>, ctx: unknown, unsubscribe: () => void) => void }) {
+    private pathByResource = new Map<string, string>();
+    private resourceByPath = new Map<string, string>();
+    constructor(options: { api: string | (() => string), createWorker: (resource: T) => Worker | SharedWorker | undefined, getProtocols?: (resource: string) => Promise<string[] | null>, autoUnsubscribe?: (this: WorkerWsSubscriptions<T>, ctx: unknown, unsubscribe: () => void) => void }) {
         this.api = options.api;
         this.createWorker = options.createWorker;
         if (options.getProtocols) {
-            this.getProtocolsProxy = Comlink.proxy(options.getProtocols);
+            const getProtocols = options.getProtocols;
+            this.getProtocolsProxy = Comlink.proxy((path: string) => {
+                const resource = this.resourceByPath.get(path)
+                return getProtocols(resource ?? '');
+            });
         }
         if (options.autoUnsubscribe) {
             this.autoUnsubscribeBinded = options.autoUnsubscribe.bind(this);
@@ -18,7 +23,7 @@ export class WorkerWsSubscriptions<T extends string = string> {
     }
     workers: Map<T, Comlink.Remote<WorkerWsController> | undefined> = new Map();
     subscriptions: Map<T, Map<unknown, { unsubscribe: () => void }>> = new Map();
-    async subscribe(ctx: unknown, resource: T, onEvent: (msg: string) => void, onConnect?: () => void) {
+    async subscribe(ctx: unknown, resource: T, onEvent: (msg: string) => void, hooks: { onConnect?: () => void, onDisconnect?: () => void, onClosed?: () => void } = {}) {
         let subscriptions = this.subscriptions.get(resource)
         if (subscriptions == null) {
             subscriptions = new Map();
@@ -37,7 +42,10 @@ export class WorkerWsSubscriptions<T extends string = string> {
                 throw new Error(`Unable to create worker for resource ${resource}`);
             }
             workerWs = Comlink.wrap<WorkerWsController>(_worker instanceof Worker ? _worker : _worker.port);
+            const path = await workerWs.path;
             this.workers.set(resource, workerWs);
+            this.pathByResource.set(resource, path);
+            this.resourceByPath.set(path, resource);
         } else {
             workerWs = currentWorker;
         }
@@ -52,10 +60,15 @@ export class WorkerWsSubscriptions<T extends string = string> {
             Comlink.proxy(pong => {
                 pong()
             }),
+            hooks.onConnect ? Comlink.proxy(() => {
+                hooks.onConnect?.();
+            }) : undefined,
+            hooks.onDisconnect ? Comlink.proxy(() => {
+                hooks.onDisconnect?.();
+            }) : undefined,
             Comlink.proxy(() => {
-                onConnect?.();
-            }),
-            Comlink.proxy(() => {
+                hooks.onConnect?.()
+                console.error('Connection closed from the worker');
                 this.subscriptions.get(resource)?.delete(ctx);
                 this.checkSubscriptions(resource);
             }),
@@ -77,6 +90,9 @@ export class WorkerWsSubscriptions<T extends string = string> {
     private checkSubscriptions(resource: T) {
         if ((this.subscriptions.get(resource)?.size) === 0) {
             this.workers.delete(resource);
+            const path = this.pathByResource.get(resource);
+            this.pathByResource.delete(resource);
+            if (path) this.resourceByPath.delete(path);
         }
     }
 }
